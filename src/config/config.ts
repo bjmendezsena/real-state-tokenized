a9713f91c7a3cb54f1411238ac7ecd2feda06e03
Briaulin Mendez <briaulin@io.builders>
2022-02-17 12:37:14 +0100

    Claves de firebase añadidas

---
+++ src/config/config.ts
@@ -0,0 +1,10 @@
+// Import the functions you need from the SDKs you need
+export const firebaseConfig = {
+  apiKey: "AIzaSyAXiwaSM7CCZM6nuk90-PQKCEvFHZUoc4Q",
+  authDomain: "real-state-tokenized-app.firebaseapp.com",
+  projectId: "real-state-tokenized-app",
+  storageBucket: "real-state-tokenized-app.appspot.com",
+  messagingSenderId: "990274734342",
+  appId: "1:990274734342:web:ac94ad65d9b9bd7138f687",
+  measurementId: "G-NR4TZ117PP",
+};
